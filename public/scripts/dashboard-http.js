be86b067f67bfc59d4e8a2d9915ced5212a4370f
RomanZakharenkoUa <115548670+RomanZakharenkoUa@users.noreply.github.com>
2025-10-29 09:45:15 +0100

    added counter badges 9

--- public/scripts/dashboard-http.js
+++ public/scripts/dashboard-http.js
@@ -87,7 +87,7 @@ class DataDashboard {
         competitorBadge.id = 'competitorCounter';
         competitorBadge.innerHTML = `
             <div class="counter-icon" style="background: #fce7f3;">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ec4899" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
                 </svg>
             </div>
@@ -102,7 +102,7 @@ class DataDashboard {
         adsBadge.id = 'adsCounter';
         adsBadge.innerHTML = `
             <div class="counter-icon" style="background: #fae8ff;">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#a855f7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#a855f7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <polygon points="22,3 2,3 10,12.46 10,19 14,21 14,12.46"></polygon>
                 </svg>
             </div>
