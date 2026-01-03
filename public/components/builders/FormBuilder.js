eba856a6e09d2711054a955dc4dc51269b6b12d2
RomanZakharenkoUa <115548670+RomanZakharenkoUa@users.noreply.github.com>
2026-01-03 18:59:49 +0100

    moved add + 10

--- public/components/builders/FormBuilder.js
+++ public/components/builders/FormBuilder.js
@@ -37,7 +37,6 @@ class FormBuilder {
                             <line x1="12" y1="5" x2="12" y2="19"></line>
                             <line x1="5" y1="12" x2="19" y2="12"></line>
                         </svg>
-                        Add
                     </button>
                     
                     <button type="button" class="run-analysis-btn" id="runAnalysisBtn">
